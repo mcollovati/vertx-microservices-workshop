f911d7ff1944a265a684db269a0e7a872fee057a
Marco Collovati <mcollovati@gmail.com>
2016-08-03 23:28:36 +0200

    wait for all components to be ready before start in JavaCompulsiveTraderVerticle

--- compulsive-traders/src/main/java/io/vertx/workshop/trader/impl/JavaCompulsiveTraderVerticle.java
+++ compulsive-traders/src/main/java/io/vertx/workshop/trader/impl/JavaCompulsiveTraderVerticle.java
@@ -9,10 +9,13 @@ import io.vertx.servicediscovery.types.MessageSource;
 import io.vertx.workshop.common.MicroServiceVerticle;
 import io.vertx.workshop.portfolio.PortfolioService;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * A compulsive trader...
  */
 public class JavaCompulsiveTraderVerticle extends MicroServiceVerticle {
+
 
     @Override
     public void start(Future<Void> future) {
@@ -29,9 +32,12 @@ public class JavaCompulsiveTraderVerticle extends MicroServiceVerticle {
         Future<PortfolioService> portfolioFuture = Future.future();
 
         // Retrieve the services, use the "special" completed to assign the future
-        MessageSource.getConsumer(discovery, new JsonObject().put("name", "market-data"),
-            marketFuture.completer());
-        EventBusService.getProxy(discovery, PortfolioService.class, portfolioFuture.completer());
+        ////MessageSource.getConsumer(discovery, new JsonObject().put("name", "market-data"),
+        ////    marketFuture.completer());
+        tryGetMarketConsumer(marketFuture);
+
+        ////EventBusService.getProxy(discovery, PortfolioService.class, portfolioFuture.completer());
+        tryGetPortfolioService(portfolioFuture);
 
         // When done (both services retrieved), execute the handler
         CompositeFuture.all(marketFuture, portfolioFuture).setHandler(ar -> {
@@ -51,6 +57,45 @@ public class JavaCompulsiveTraderVerticle extends MicroServiceVerticle {
                 future.complete();
             }
         });
+
+    }
+
+
+    private void tryGetMarketConsumer(Future<MessageConsumer<JsonObject>> marketFuture) {
+        AtomicInteger retry = new AtomicInteger(5);
+        MessageSource.<JsonObject>getConsumer(discovery, new JsonObject().put("name", "market-data"), ar -> {
+            if (ar.failed()) {
+                if (retry.decrementAndGet() < 0) {
+                    marketFuture.fail(ar.cause());
+                } else {
+                    vertx.setTimer(1000, l -> {
+                        vertx.cancelTimer(l);
+                        tryGetMarketConsumer(marketFuture);
+                    });
+                }
+            } else {
+                marketFuture.complete(ar.result());
+            }
+        });
+    }
+
+    private void tryGetPortfolioService(Future<PortfolioService> portfolioFuture) {
+        AtomicInteger retry = new AtomicInteger(5);
+        EventBusService.getProxy(discovery, PortfolioService.class, ar -> {
+            if (ar.failed()) {
+                if (retry.decrementAndGet() < 0) {
+                    portfolioFuture.fail(ar.cause());
+                } else {
+                    vertx.setTimer(1000, l -> {
+                        vertx.cancelTimer(l);
+                        tryGetPortfolioService(portfolioFuture);
+                    });
+                }
+            } else {
+                portfolioFuture.complete(ar.result());
+            }
+        });
+        ;
     }
 
 
